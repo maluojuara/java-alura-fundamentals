b59049cffcdf22d0cc761ecf372bae8bbe78301e
maluojuara <araujomaluc@gmail.com>
2024-07-24 18:41:16 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Screen Match");
-        System.out.println("Movie: Top Gun");
+        System.out.println("Hello World!");
     }
 }
