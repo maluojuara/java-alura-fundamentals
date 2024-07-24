afa0e5b92e72d48171e50af507045a00872bf9cd
maluojuara <araujomaluc@gmail.com>
2024-07-24 18:28:21 -0300

    aula 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Screen Match");
+        System.out.println("Movie: Top Gun");
+
+        int year = 2022;
+    }
+}
